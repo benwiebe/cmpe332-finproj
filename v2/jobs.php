9e208f4f37eb9729402b51381165d0d55f7dbadf
gavin_mcclelland <gavinmcclelland65@gmail.com>
2019-03-19 19:34:25 -0400

    Jobs well done. Haha. Get it? I need a beer

--- v2/jobs.php
+++ v2/jobs.php
@@ -4,6 +4,11 @@
 	pixelarity.com | hello@pixelarity.com
 	License: pixelarity.com/license
 -->
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=project', "root", "");
+
+?>
+
 <html>
 	<head>
 		<title>CMPE 332 Project</title>
@@ -34,12 +39,50 @@
 
 						<!-- Content -->
 							<section id="content" class="main">
-								<span class="image main"><img src="images/pic04.jpg" alt="" /></span>
-								<h2>Magna feugiat lorem</h2>
-								<p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-								<p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-								<h2>Tempus veroeros</h2>
-								<p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+							<form method="post">
+									<legend>Select a Company</legend>
+									<select name="company">
+										<option value="all">View All Jobs</option>
+										<?php
+											$sql = "select name from company";
+											$stmt = $pdo->prepare($sql);   #create the query
+											$stmt->execute([]);   #bind the parameters
+
+											#stmt contains the result of the program execution
+											#use fetch to get results row by row.
+											while ($row = $stmt->fetch()) {
+												echo "<option value='".$row['name']."'>".$row['name']."</option>";
+											}
+
+										?>
+									</select>
+									<input type="submit" value="Select Company">
+								</form>
+								<br />
+
+								<table>
+									<h2>Jobs Available<h2>
+									<tr><th>Title</th><th>Province</th><th>City</th><th>Payrate</th><th>Company</th></tr>
+										<?php
+											if(isset ($_POST['company']))
+												$company = $_POST['company'];
+											
+											$sql = "select * from posted_job";
+											if($company!="all")
+												$sql = $sql . " where company = ?";
+												$stmt = $pdo->prepare($sql);   #create the query
+											if($company!="all")
+												$stmt->execute([$company]);   #bind the parameters
+											else
+												$stmt->execute([]);
+
+											while ($row = $stmt->fetch()) {
+												echo "<tr><td>".$row["title"]."</td><td>".$row["province"]."</td><td>".$row["city"]."</td><td>".$row["payrate"]."</td><td>".$row["company"]."</td></tr>";
+											}
+											
+										?>
+								</table>
+
 							</section>
 
 					</div>
